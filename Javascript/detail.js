883384670db7c8d63a4c07339f9851c31d970895
SLaks <Schabse@Gmail.com>
2012-10-03 19:28:18 -0400

    In the beginning...

---
+++ Javascript/detail.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="../Scripts/modernizr-2.6.1-respond-1.1.0.min.js" />
+/// <reference path="../Scripts/jquery-1.8.2.js" />
+/// <reference path="../Scripts/knockout-2.1.0.js" />
+
+var data = [
+	{ author: 'Schabse Laks', text: 'Welcome to the CalendarBoard system!' }
+];
