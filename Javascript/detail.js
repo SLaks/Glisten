a40c9defab63b25eb6f3d04b431551cbd868c67e
SLaks <Schabse@Gmail.com>
2012-10-03 20:26:58 -0400

    Add list & coloring for detail

--- Javascript/detail.js
+++ Javascript/detail.js
@@ -2,6 +2,20 @@
 /// <reference path="../Scripts/jquery-1.8.2.js" />
 /// <reference path="../Scripts/knockout-2.1.0.js" />
 
+var colorCount = 5;
+
 var data = [
-	{ author: 'Schabse Laks', text: 'Welcome to the CalendarBoard system!' }
+	{ author: 'Schabse Laks', text: 'Welcome to the CalendarBoard system!' },
+	{ author: 'Chana Laks', text: 'We aren\'t having anyone for שבת' },
+	{ author: 'David Laks', text: 'There are two Kiddushim to go to.' },
 ];
+
+$.each(data, function () {
+	var hash = 17;
+	for (var i = 0; i < this.text.length; i++) {
+		hash = hash * 7 + this.text.charCodeAt(i);
+	}
+	this.cssClass = "Color" + (1 + (hash % colorCount));
+});
+
+ko.applyBindings({ messages: data });
