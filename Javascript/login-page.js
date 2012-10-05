7de9daecea30753aafbfca7d5aae4ae1a1c003dc
SLaks <Schabse@Gmail.com>
2012-10-05 09:58:24 -0400

    Add login page

---
+++ Javascript/login-page.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="../Scripts/jquery-1.8.2.js" />
+/// <reference path="../Scripts/knockout-2.1.0.js" />
+/// <reference path="providers.js" />
+
+var loginOptions = [];
+
+for (var id in CalenderBoard.providers) {
+	if (!CalenderBoard.providers.hasOwnProperty(id)) continue;
+	var p = CalenderBoard.providers[id];
+	if (typeof p.login !== 'function') continue;
+
+	loginOptions.push({
+		id: id,
+		name: p.displayName
+	});
+}
+ko.applyBindings({ loginOptions: loginOptions });
---
+++ Javascript/providers.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="../Scripts/jquery-1.8.2.js" />
+
+//Providers are used by all three pages:
+//index.html enumerates providers from the properties of the providers object, and initiates login
+//lists.html displays the available lists (single-list sources, like Twitter, skip this)
+//detail.html reads the contents of a single list.
+
+//provider-themes.less should have the styling for the login buttons;
+//each button will have the property name added as a class.
+
+(function (global, undefined) {
+	var CalenderBoard = global.CalenderBoard = global.CalenderBoard || {};
+
+	CalenderBoard.providers = {
+		trello: {
+			displayName: 'Trello',
+			login: function () { }
+		},
+		gCalendar: {
+			displayName: 'Google Calendar',
+			login: function () { }
+		}
+	};
+}(this));
