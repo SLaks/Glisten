6ca338198f18c36abfe00d612d89d6329096e256
SLaks <Schabse@Gmail.com>
2012-10-09 22:52:33 -0400

    Start select page

---
+++ Javascript/select-page.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="../Scripts/jquery-1.8.2.js" />
+/// <reference path="../Scripts/knockout-2.1.0.js" />
+/// <reference path="providers.js" />
+
+var providerId = location.search.match(/[?&]source=([^&]*)/i)[1];
+var provider = Glisten.providers[providerId];
+
+var viewModel = {
+	providerId: providerId,
+	groups: ko.observableArray(),
+	isLoading: ko.observable(true)
+};
+
+ko.applyBindings(viewModel);
+
+function refresh() {
+	viewModel.isLoading(true);
+
+}
