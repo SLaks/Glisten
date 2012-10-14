3c600df0ca3a92462d45d193ae52c675340990cd
SLaks <Schabse@Gmail.com>
2012-10-14 18:45:19 -0400

    Start Google Calendar

--- Javascript/providers.js
+++ Javascript/providers.js
@@ -1,5 +1,5 @@
 ﻿/// <reference path="../Scripts/jquery-1.8.2.js" />
-/// <reference path="https://api.trello.com/1/client.js?key=6068fb37739492894da608debf94ef7f" />
+/// <reference path="https://api.trello.com/1/client.js" />
 
 //Providers are used by all three pages:
 //index.html enumerates providers from the properties of the providers object, and initiates login
@@ -10,15 +10,42 @@
 //each button will have the property name added as a class.
 
 (function (global, undefined) {
+	var trelloKey = '6068fb37739492894da608debf94ef7f';
+	var googleKey = 'AIzaSyB6WakYj2GCQIudxb_e7X9pZ8M86Eqw8bE';
+
 	var loadedScripts = {};
 	function loadScript(url) {
-		/// <summary>Asynchronously loads an external .js file.</summary>
+		/// <summary>Asynchronously loads an external .js file, if it has not been loaded already.</summary>
 		/// <param name="url" type="String">The URL of the script file to load.</param>
 		/// <returns type="jqXHR" />
 		if (!loadedScripts.hasOwnProperty(url))
 			loadedScripts[url] = $.getScript(url);
 		return loadedScripts[url];
 	}
+
+	var googleApis = {};
+	function loadGoogleApi(name, version) {
+		/// <summary>Asynchronously loads a Google Javascript API.</summary>
+		/// <param name="name" type="String">The name of the API to load.</param>
+		/// <param name="version" type="String">The version of the API to load.</param>
+		/// <returns type="$.Deferred" />
+		var promiseKey = name + "/" + version;
+
+		if (!googleApis.hasOwnProperty(promiseKey)) {
+			googleApis[promiseKey] = loadScript("https://apis.google.com/js/client.js").pipe(function () {
+				gapi.client.setApiKey(googleKey);
+
+				var retVal = $.Deferred();
+
+				gapi.client.load(name, version, function () { retVal.resolve(); });
+
+				return retVal.promise();
+			});
+		}
+
+		return googleApis[promiseKey];
+	}
+
 
 	var Glisten = global.Glisten = global.Glisten || {};
 
@@ -63,7 +90,7 @@
 			login: function () {
 				var deferred = $.Deferred();
 
-				loadScript('https://api.trello.com/1/client.js?key=6068fb37739492894da608debf94ef7f')
+				loadScript('https://api.trello.com/1/client.js?key=' + encodeURIComponent(trelloKey))
 					.then(function () {
 						Trello.authorize({
 							type: 'popup',
@@ -82,10 +109,54 @@
 		gCalendar: {
 			displayName: 'Google Calendar',
 			getLists: function () {
+
+				return this.login().pipe(function () {
+					var promise = $.Deferred();
+
+					gapi.client.calendar.calendarList.list(
+						{ fields: "items(id,summary,summaryOverride)" },
+						function (results) {
+							promise.resolve(results);
+						}
+					);
+
+					return promise.promise();
+				}).pipe(function (results) {
+					var groups = [];
+					for (var i = 0; i < results.items.length; i++) {
+						var calendar = results.items[i];
+
+						groups.push({
+							name: calendar.summaryOverride || calendar.summary,
+							items: [
+								{ name: 'This Week', id: '1w:' + calendar.id },
+								{ name: 'This Month', id: '1m:' + calendar.id }
+							]
+						});
+					}
+				});
 			},
 			readList: function (id) {
 			},
-			login: function () { }
+			login: function () {
+				return loadGoogleApi('calendar', 'v3').pipe(function () {
+					var promise = $.Deferred();
+
+					gapi.auth.authorize(
+						{
+							client_id: 'AIzaSyB6WakYj2GCQIudxb_e7X9pZ8M86Eqw8bE',
+							scope: 'https://www.googleapis.com/auth/calendar.readonly',
+							immediate: true
+						}, function (authResult) {
+							if (authResult && authResult.error)
+								promise.reject(authResult.error);
+							else
+								promise.resolve();
+						});
+
+					return promise.promise();
+				});
+			}
 		}
 	};
 }(this));
