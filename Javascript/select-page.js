9f3358402cd3f6404f5695cd8e6c0b9415c34500
SLaks <Schabse@Gmail.com>
2012-10-14 12:44:11 -0400

    Display lists in select page

--- Javascript/select-page.js
+++ Javascript/select-page.js
@@ -10,10 +10,33 @@ var viewModel = {
 	groups: ko.observableArray(),
 	isLoading: ko.observable(true)
 };
+if (!provider)
+	location = "..";
+else {
+	ko.applyBindings(viewModel);
 
-ko.applyBindings(viewModel);
-
+	refresh();
+}
 function refresh() {
 	viewModel.isLoading(true);
+	provider.getLists().then(function (groups) {
+		viewModel.groups.removeAll();
 
+		for (var g = 0; g < groups.length; g++) {
+			var source = groups[g];
+			var group = { name: source.name, lists: [] };
+
+			for (var i = 0; i < source.lists.length; i++) {
+				var list = source.lists[i];
+				group.lists.push({
+					name: list.name,
+					url: "list.html?source=" + encodeURIComponent(providerId) + "&list=" + encodeURIComponent(list.id)
+				});
+			}
+
+			viewModel.groups.push(group);
+		}
+
+		viewModel.isLoading(false);
+	});
 }
